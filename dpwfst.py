e76caf25227ac53ef25f84a8cfae8a55624b552a
Nicol Visser <nicolvisser@yahoo.com>
2025-02-07 23:02:56 +0200

    Update dpwfst.py

--- dpwfst.py
+++ dpwfst.py
@@ -1,59 +1,74 @@
-from typing import Optional
+from typing import List, Optional, Union
 
 import k2
 import torch
 
 
 def dpwfst(
-    features: torch.Tensor,
+    features: Union[torch.Tensor, List[torch.Tensor]],
     codebook: torch.Tensor,
     lmbda: float,
     num_neighbors: Optional[int] = None,
+    deduplicate: Optional[bool] = False,
 ):
-    if features.dim() != 2:
-        raise NotImplementedError("Only works for 2D input")
-    
+    if isinstance(features, torch.Tensor):
+        # if unbatched, make batched
+        features = [features]
+
+    # build fsa for each set of features
+    fsas = [
+        build_fsa(features, codebook, num_neighbors, lmbda, deduplicate)
+        for features in features
+    ]
+    # create fsa vector so that we can process in parallel
+    fsa_vec = k2.create_fsa_vec(fsas)
+    # Find the shortest path(s) with tropical semiring
+    best = k2.shortest_path(fsa_vec, use_double_scores=True)
+    # Split back into individual FSAs
+    split_fsas = [best[i] for i in range(len(features))]
+    # Get units for each FSA
+    units = [fsa.aux_labels[fsa.aux_labels != -1] for fsa in split_fsas]
+
+    return units
+
+
+def build_fsa(features, codebook, num_neighbors, lmbda, deduplicate):
     assert features.device == codebook.device
     device = features.device
 
     if num_neighbors is None:
         num_neighbors = codebook.shape[0]
 
-    distances = torch.cdist(features, codebook, p=2.0) ** 2
+    distances = torch.cdist(features, codebook, p=2.0) ** 2  # uses squared euclidean
     top_k_distances, top_k_indices = torch.topk(
         distances, k=num_neighbors, dim=1, largest=False
     )
 
-    arcs1, labels1, scores1 = build_initial_transition_arcs(
+    arcs1, out_labels1, scores1 = build_initial_transition_arcs(
         top_k_distances, top_k_indices
     )
-    arcs2, labels2, scores2 = build_intermediate_transition_arcs(
-        top_k_distances, top_k_indices, lmbda
+    arcs2, out_labels2, scores2 = build_intermediate_transition_arcs(
+        top_k_distances, top_k_indices, lmbda, deduplicate
     )
-    arcs3, labels3, scores3 = build_final_transition_arcs(
+    arcs3, out_labels3, scores3 = build_final_transition_arcs(
         T=distances.shape[0], k=num_neighbors, device=device
     )
 
     arcs = torch.cat([arcs1, arcs2, arcs3], dim=0)
-    labels = torch.cat([labels1, labels2, labels3], dim=0)
+    out_labels = torch.cat([out_labels1, out_labels2, out_labels3], dim=0)
     scores = torch.cat([scores1, scores2, scores3], dim=0)
 
-    # Create FSA
+    # Create FSA (includes dummy int32 scores)
     fsa = k2.Fsa(arcs)
-
     # Now add the true float scores
     fsa.scores = scores
+    # Attach the auxilary labels. This can be anything.
+    # If you want you could attach the codebook tensors here.
+    # This would then give you the quantized features as output.
+    # For now, to obtain units, we can use the output labels of the WFST.
+    fsa.aux_labels = out_labels
 
-    # Define the auxilary labels. This can be anything. Simply using labels for now.
-    fsa.aux_labels = labels
-
-    fsa_vec = k2.create_fsa_vec([fsa])
-
-    # Shortest path
-    best = k2.shortest_path(fsa_vec, use_double_scores=True)
-    units = best.aux_labels[:-1]
-
-    return units
+    return fsa
 
 
 def build_initial_transition_arcs(top_k_distances, top_k_indices):
@@ -68,7 +83,9 @@ def build_initial_transition_arcs(top_k_distances, top_k_indices):
     return arcs, labels, scores
 
 
-def build_intermediate_transition_arcs(top_k_distances, top_k_indices, lmbda):
+def build_intermediate_transition_arcs(
+    top_k_distances, top_k_indices, lmbda, deduplicate=False
+):
     T, k = top_k_indices.shape
     device = top_k_indices.device
     src = (
@@ -88,15 +105,20 @@ def build_intermediate_transition_arcs(top_k_distances, top_k_indices, lmbda):
     labels = (
         top_k_indices[1:].to(torch.int32).unsqueeze(1).expand(T - 1, k, k).flatten()
     )
-    quant_scores = -top_k_distances[1:].unsqueeze(1).expand(T - 1, k, k).flatten()
-    duration_scores = (
+    repeats_unit = (
         top_k_indices[:-1].unsqueeze(-1).expand(T - 1, k, k).flatten()
         == top_k_indices[1:].unsqueeze(1).expand(T - 1, k, k).flatten()
-    ).to(torch.float32) * lmbda
-    scores = quant_scores + duration_scores
+    )
+    aux_labels = labels.clone()
+    if deduplicate:
+        # then use epsilon transitions if the same unit is repeated
+        aux_labels[repeats_unit] = -1
+    quant_penalty = top_k_distances[1:].unsqueeze(1).expand(T - 1, k, k).flatten()
+    duration_bonus = repeats_unit.to(torch.float32) * lmbda
+    scores = -quant_penalty + duration_bonus
     dummy_scores = torch.zeros_like(scores, dtype=torch.int32, device=device)
     arcs = torch.stack([src, dest, labels, dummy_scores], dim=1)
-    return arcs, labels, scores
+    return arcs, aux_labels, scores
 
 
 def build_final_transition_arcs(T, k, device):
